afe33e2774debca2d58db4c636751b6e58325926
Bé Bự <nl98hk@gmail.com>
2019-08-20 12:39:56 +0700

    Update #8

--- Project/src/app/models/products.model.ts
+++ Project/src/app/models/products.model.ts
@@ -1,6 +1,6 @@
 export class Product{
     constructor(
-        public id: number,
+        public id: number,//timestamp
         public title: string,
         public price: number,
         public oldPrice: number,
